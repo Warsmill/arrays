8a24e714946ed45ba6836788de790d9a56d5a12a
warsmill <warsmill@yandex.ru>
2025-04-02 12:24:11 +0300

    array elements in order

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,28 @@ public class Main {
         int[] number1 = new int[]{1, 2, 3};
         double[] number2 = new double[]{1.57, 7.654, 9.986};
         String[] word = new String[]{"Backend", "разработчик", "Java"};
+
+
+        for(int i = 0; i < number1.length; i++){
+            System.out.print(number1[i]);
+            if(i < number1.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for(int i = 0; i < number2.length; i++){
+            System.out.print(number2[i]);
+            if (i < number2.length - 1){
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for(int i = 0; i < word.length; i++){
+            System.out.print(word[i]);
+            if (i < word.length - 1){
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
     }
 }
