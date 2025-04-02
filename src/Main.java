3130368f256ea967b6f3ed6f2f26a5a1dcdfd351
warsmill <warsmill@yandex.ru>
2025-04-02 12:22:13 +0300

    declaration of arrays

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,5 @@ public class Main {
         int[] number1 = new int[]{1, 2, 3};
         double[] number2 = new double[]{1.57, 7.654, 9.986};
         String[] word = new String[]{"Backend", "разработчик", "Java"};
-
     }
 }
